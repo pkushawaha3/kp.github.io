a13c6bebc7bad011cde379c6a289a13716d7a421
Kushwaha Prashant <kushwahaprashant143@gmail.com>
2020-02-11 00:00:40 +0530

    Add files via upload

---
+++ PackageTest.java
@@ -0,0 +1,7 @@
+package com.nt.corejava;
+class PackageTest{
+      public static void main(String[] args){
+            System.out.println("Get Package All Errors");
+           
+       }//main
+}//class
